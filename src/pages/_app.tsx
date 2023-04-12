81e7e0d8e3cbd5e1783e785d57878ef64bd413f2
justinwhall <justinwhall@me.com>
2023-04-12 13:30:35 -0600

    remove not needed provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '@/lib/theme';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
